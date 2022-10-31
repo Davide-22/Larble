c786dcb57b411b6ffbd4a544bc1e85881e56f655
Stefano <servillostefano@gmail.com>
2022-10-31 23:10:49 +0100

    [Done] single player page

---
+++ app/src/main/java/com/example/larble/SinglePlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.larble
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SinglePlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_singleplayer)
+
+    }
+}
