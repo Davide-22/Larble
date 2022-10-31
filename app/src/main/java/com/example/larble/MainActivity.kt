bfca05479d27520806626aba817ec70fc1fd3cbb
Stefano <servillostefano@gmail.com>
2022-10-31 23:11:28 +0100

    [Done] main page + background

--- app/src/main/java/com/example/larble/MainActivity.kt
+++ app/src/main/java/com/example/larble/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.larble
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private var login: Button? = null
+    private var signUp: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login = findViewById(R.id.login)
+        signUp = findViewById(R.id.sign_up)
+
+        login?.setOnClickListener {
+            val email: EditText = findViewById(R.id.editTextTextEmailAddress)
+            intent = Intent(this, MenuActivity::class.java)
+            intent.putExtra("email",email.text.toString())
+            startActivity(intent)
+        }
+
+        signUp?.setOnClickListener {
+            intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
