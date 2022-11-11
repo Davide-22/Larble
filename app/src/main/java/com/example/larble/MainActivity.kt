b79ea1dc24e05daeb77822dc531b1affd019e03a
Stefano <servillostefano@gmail.com>
2022-11-11 22:42:24 +0100

    [Edit] Insert verify in the loading page

--- app/src/main/java/com/example/larble/MainActivity.kt
+++ app/src/main/java/com/example/larble/MainActivity.kt
@@ -2,72 +2,16 @@ package com.example.larble
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Patterns
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-
 class MainActivity : AppCompatActivity() {
-    private var login: Button? = null
-    private var signUp: Button? = null
-    private var email: EditText? = null
-    private var password: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        login = findViewById(R.id.login)
-        signUp = findViewById(R.id.sign_up)
-        email = findViewById(R.id.editTextTextEmailAddress)
-        password = findViewById(R.id.editTextTextPassword)
-
-        login?.setOnClickListener {
-            if (Patterns.EMAIL_ADDRESS.matcher(email!!.text.toString()).matches() && password!!.text.isNotEmpty()){
-                intent = Intent(this, MenuActivity::class.java)
-                val requestModel = LoginRequestModel(email!!.text.toString(),password!!.text.toString())
-
-                val response = ServiceBuilder.buildService(APIInterface::class.java)
-                response.requestLogin(requestModel).enqueue(
-                    object: Callback<LoginResponseClass> {
-                        override fun onResponse(
-                            call: Call<LoginResponseClass>,
-                            response: Response<LoginResponseClass>
-                        ){
-                            if(response.body()!!.status=="true"){
-                                intent.putExtra("username", response.body()!!.username)
-                                val sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE)
-                                val myEdit = sharedPreferences.edit()
-                                myEdit.putString("token", response.body()!!.msg)
-                                myEdit.putString("username", response.body()!!.username)
-                                myEdit.apply()
-                                startActivity(intent)
-                            }else{
-                                Toast.makeText(this@MainActivity, response.body()!!.msg, Toast.LENGTH_LONG)
-                                    .show()
-                            }
-                        }
-                        override fun onFailure(call: Call<LoginResponseClass>, t: Throwable) {
-                            Toast.makeText(this@MainActivity, t.toString(), Toast.LENGTH_LONG)
-                                .show()
-                        }
-                    }
-                )
-
-            }else{
-                Toast.makeText(applicationContext, "Insert valid email and password", Toast.LENGTH_LONG).show()
-            }
-        }
-
-        signUp?.setOnClickListener {
-            intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
-        }
-
     }
 
     override fun onResume() {
@@ -76,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun verify(){
-        intent = Intent(this, MenuActivity::class.java)
+        intent = Intent(this@MainActivity, LoginActivity::class.java)
         val sh = getSharedPreferences("MySharedPref", MODE_PRIVATE)
         val token: String? = sh.getString("token", "")
         val requestModel = token?.let { VerifyRequestModel(it) }
@@ -90,8 +34,9 @@ class MainActivity : AppCompatActivity() {
                         response: Response<ResponseClass>
                     ){
                         if(response.body()!!.status=="true"){
-                            startActivity(intent)
+                            intent = Intent(this@MainActivity, MenuActivity::class.java)
                         }
+                        startActivity(intent)
                     }
                     override fun onFailure(call: Call<ResponseClass>, t: Throwable) {
                         Toast.makeText(this@MainActivity, t.toString(), Toast.LENGTH_LONG)
@@ -101,5 +46,4 @@ class MainActivity : AppCompatActivity() {
             )
         }
     }
-
 }
