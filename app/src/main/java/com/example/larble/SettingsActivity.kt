5330fba8f21d3cc63c57b18b382ddbbd297b0600
Stefano <servillostefano@gmail.com>
2022-11-11 11:09:34 +0100

    [Edit] Logout button

--- app/src/main/java/com/example/larble/SettingsActivity.kt
+++ app/src/main/java/com/example/larble/SettingsActivity.kt
@@ -1,11 +1,26 @@
 package com.example.larble
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
+    private var logout: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        logout = findViewById(R.id.log_out)
+
+        logout?.setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            val sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE)
+            val myEdit = sharedPreferences.edit()
+            myEdit.putString("token", "")
+            myEdit.putString("username", "")
+            myEdit.apply()
+            startActivity(intent)
+        }
+
     }
 }
