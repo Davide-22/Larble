e1033d47a2e3a262e2701df87d9378c3a797975a
Stefano <servillostefano@gmail.com>
2022-12-11 15:54:27 +0100

    [EDIT] change color second ball

--- app/src/main/java/com/example/larble/BallView.kt
+++ app/src/main/java/com/example/larble/BallView.kt
@@ -1,16 +1,17 @@
 package com.example.larble
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
+import android.graphics.*
 import android.view.View
+import androidx.core.content.ContextCompat
 
 class BallView(context: Context) : View(context) {
-    val ballSrc = BitmapFactory.decodeResource(resources, R.drawable.ball2)
-    var ball : Bitmap = Bitmap.createScaledBitmap(ballSrc, 100, 100, true)
+    private val ballSrc: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.ball2)
+    private var ball : Bitmap = Bitmap.createScaledBitmap(ballSrc, 100, 100, true)
     var bitmaps: MutableList<Bitmap> = arrayListOf(ball)
     var positions= FloatArray(4){1.0f}
+    private var paint: Paint = Paint()
+    private val filter: ColorFilter = PorterDuffColorFilter(ContextCompat.getColor(context, R.color.red), PorterDuff.Mode.SRC_IN)
 
     fun setParam(x: Float, y: Float) {
         positions[0] = x
@@ -20,7 +21,13 @@ class BallView(context: Context) : View(context) {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         for(i in 0 until bitmaps.size){
-            canvas.drawBitmap(bitmaps[i], positions[2*i], positions[(2*i)+1], null)
+            if(i==0){
+                canvas.drawBitmap(bitmaps[i], positions[0], positions[1], null)
+            }else{
+                paint.colorFilter = filter
+                paint.alpha = 50
+                canvas.drawBitmap(bitmaps[i], positions[2*i], positions[(2*i)+1], paint)
+            }
         }
         invalidate()
     }
